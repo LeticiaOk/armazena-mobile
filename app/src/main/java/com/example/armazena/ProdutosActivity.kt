543f8e708a6c6a574b50951f619541594a3bb67d
leticiaOk <leticiaalves6540@gmail.com>
2025-02-23 20:51:18 -0300

    criação do projeto com login do usuário

---
+++ app/src/main/java/com/example/armazena/ProdutosActivity.kt
@@ -0,0 +1,20 @@
+package com.example.armazena
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProdutosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_produtos)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
